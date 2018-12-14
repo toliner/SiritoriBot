93d4c678b777576adc39eab84ba070e73b1a633e
トリナー <toliner.rkt@gmail.com>
2018-12-14 23:25:25 +0900

    add config of save period

--- src/main/kotlin/toliner/discord/siritori/ConfigData.kt
+++ src/main/kotlin/toliner/discord/siritori/ConfigData.kt
@@ -3,13 +3,17 @@ package toliner.discord.siritori
 import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 
+const val FIVE_MINUTE = 5L * 60L * 1000L
+
 @Serializable
 data class ConfigData(
     val guild: GuildInfo,
     val token: String,
     @Optional
     val gameMessage: String = "しりとり",
-    val plugins: List<String>
+    val plugins: List<String>,
+    @Optional
+    val savePeriod: Long = FIVE_MINUTE
 )
 
 @Serializable
