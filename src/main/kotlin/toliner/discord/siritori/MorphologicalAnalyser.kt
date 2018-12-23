139789341feb2165d2ff11202e8f6de2b83c9633
トリナー <toliner.rkt@gmail.com>
2018-12-24 01:10:50 +0900

    reconstruct MorphologicalAnalyser

--- src/main/kotlin/toliner/discord/siritori/MorphologicalAnalyser.kt
+++ src/main/kotlin/toliner/discord/siritori/MorphologicalAnalyser.kt
@@ -1,50 +1,40 @@
 package toliner.discord.siritori
 
 import com.worksap.nlp.sudachi.DictionaryFactory
-import com.worksap.nlp.sudachi.Morpheme
 import com.worksap.nlp.sudachi.Tokenizer
 import java.io.File
 import java.io.PrintStream
 
+data class AnalyzeResult(
+    val original: String,
+    val normalizedForm: String,
+    val readingForm: String,
+    val separated: List<String>,
+    val parts: List<List<String>>
+)
+
 object MorphologicalAnalyser {
     private val dict = DictionaryFactory().create(ClassLoader.getSystemResource("sudachi_fulldict.json").readText())!!
     private val tokenizer = dict.create()!!.apply {
-        setDumpOutput(PrintStream(File("logs/sudachi.log").createIfNotExsist()))
+        setDumpOutput(PrintStream(File("logs/sudachi.log").createIfNotExists()))
     }
 
-    fun analyze(word: String): List<Morpheme> {
-        return tokenizer.tokenize(Tokenizer.SplitMode.C, word)
+    fun analyze(word: String): AnalyzeResult {
+        val tokens = tokenizer.tokenize(Tokenizer.SplitMode.C, word)
+        return AnalyzeResult(
+            original =  word,
+            normalizedForm =  tokens.joinToString("") { it.normalizedForm() },
+            readingForm = tokens.joinToString(""){ it.readingForm() },
+            separated = tokens.map { it.surface() },
+            parts = tokens.map { it.partOfSpeech() }
+        )
     }
-
-    fun getNormalized(tokens: List<Morpheme>): String {
-        return tokens.joinToString(separator = "") {
-            it.normalizedForm()
-        }
-    }
-
-    fun getNormalized(word: String) = getNormalized(analyze(word))
-
-    fun getReadingForm(tokens: List<Morpheme>): String {
-        return tokens.joinToString(separator = "") {
-            it.readingForm()
-        }
-    }
-
-    fun getReadingForm(word: String) = getReadingForm(tokenizer.tokenize(word))
 }
 
-fun String.isSingleWord(): Boolean {
-    return MorphologicalAnalyser.analyze(this).size == 1
-}
+fun AnalyzeResult.isSingleWord() = separated.size == 1
+fun AnalyzeResult.isNoun() = isSingleWord() && parts[0][0] == "名詞"
 
-fun String.isSingleNoun(): Boolean {
-    val tokens = MorphologicalAnalyser.analyze(this)
-    if(tokens.size != 1) return false
-    val token = tokens[0]
-    return token.partOfSpeech()[0] == "名詞"
-}
-
-fun File.createIfNotExsist(): File {
+fun File.createIfNotExists(): File {
     if (!exists()) {
         createNewFile()
     }
